77699bf434f1cbbd35572d482a2a26c7b2fb2bd7
MikeSZ97 <miguelsz9711@gmail.com>
2018-05-07 17:01:07 -0500

    proyecto kata

---
+++ src/codekata/CodeKata.java
@@ -0,0 +1,13 @@
+
+package codekata;
+
+public class CodeKata {
+
+    public static void main(String[] args) {
+        Solucion S = new Solucion();
+        String salida = S.lectura(2016);
+        System.out.println(salida);
+        
+    }
+    
+}
